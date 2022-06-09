66c2eddca55ec90700e0efc72344ba2a2c474184
Julia Loh JM <julia_loh_jm@aiap.sg>
2022-06-09 13:19:15 +0800

    Upload/download module using polyaxon

--- src/streamlit.py
+++ src/streamlit.py
@@ -2,12 +2,17 @@ import os
 import logging
 import hydra
 import streamlit as st
-
-import aiap_team6_miniproject as team6_miniproject
+import aiap_dsp_mlops as amlo
+from PIL import Image
 
 @st.cache(allow_output_mutation=True)
 def load_model(model_path):
-    return team6_miniproject.modeling.utils.load_model(model_path)
+    return amlo.modeling.utils.load_model(model_path)
+
+@st.cache(allow_output_mutation=True)
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
 
 @hydra.main(config_path="../conf/base", config_name="pipelines.yml")
 def main(args):
@@ -24,29 +29,34 @@ def main(args):
     logger_config_path = os.path.\
         join(hydra.utils.get_original_cwd(),
             "conf/base/logging.yml")
-    team6_miniproject.general_utils.setup_logging(logger_config_path)
+    amlo.general_utils.setup_logging(logger_config_path)
 
     logger.info("Loading the model...")
     pred_model = load_model(args["inference"]["model_path"])
 
     logger.info("Loading dashboard...")
-    title = st.title('AIAP Team 6 Mini Project')
+    
+    st.subheader("AIAP Team 6")
+    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    if image_file is not None:
 
-    text_input = st.text_area("Review",
-        placeholder="Insert your review here")
+        # To See details
+        file_details = {"filename":image_file.name, "filetype":image_file.type,
+                            "filesize":image_file.size}
+        st.write(file_details)
 
-    if st.button("Get sentiment"):
+        # To View Uploaded Image
+        st.image(load_image(image_file))
+
+        st.download_button(
+        label="Download image",
+        data=image_file,
+        file_name="imagename.png",
+        mime="image/png")
+        
+    if st.button("Processed"):
         logger.info("Conducting inferencing on text input...")
-        curr_pred_result = float(pred_model.predict([text_input])[0])
-        sentiment = ("positive" if curr_pred_result > 0.5
-                    else "negative")
-        logger.info(
-            "Inferencing has completed. Text input: {}. Sentiment: {}"
-            .format(text_input, sentiment))
-        st.write("The sentiment of the review is {}."
-            .format(sentiment))
     else:
         st.write("Awaiting a review...")
-
 if __name__ == "__main__":
     main()
