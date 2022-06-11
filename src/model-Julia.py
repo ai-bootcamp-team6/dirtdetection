b0a9af846166c6c567db735285da41c29af7b417
Julia Loh JM <julia_loh_jm@aiap.sg>
2022-06-12 02:10:57 +0800

    Working predictions <return result dir> for fastapi and streamlit deployment

---
+++ src/model-Julia.py
@@ -0,0 +1,141 @@
+
+from fileinput import filename
+import os
+import logging
+import fastapi
+import uuid
+#from fastapi.responses import FileResponse
+
+# import aiap_team6_miniproject_fastapi as team6_miniproject_fapi
+# import aiap_team6_miniproject.data_prep.process_image as process_image
+from PIL import Image
+# from utils import read_imagefile
+from fastapi import UploadFile, File, Form, Response
+from io import BytesIO
+import shutil
+import shutil
+import uuid
+from pathlib import Path
+from typing import List
+from infer import Inference
+
+# logger = logging.getLogger(__name__)
+ROUTER = fastapi.APIRouter()
+# PRED_MODEL = team6_miniproject_fapi.deps.PRED_MODEL
+#################################Julia working codes########################
+  
+@ROUTER.post("/preprocess/image", status_code=fastapi.status.HTTP_200_OK)
+async def preprocess_api(file: UploadFile=File(...)): # place holder for image preprocessing 
+    """Endpoint that takes in the image from user upload and preprocess it for
+    training or inference.
+    
+    Parameters
+    ----------
+    image : Image that user upload for training or inference.
+
+    Returns
+    -------
+    str
+        address of the preprocessed image
+    """
+    WORK_DIR = os.getcwd()
+    # UUID to prevent file overwrite
+    # 'beautiful' path concat instead of WORK_DIR + '/' + REQUEST_ID
+    WORKSPACE = WORK_DIR
+    if not os.path.exists(WORKSPACE):
+        # recursively create workdir/unique_id
+        os.makedirs(WORKSPACE)
+    # iterate through all uploaded files
+    file.filename = f"{uuid.uuid4()}.jpg"
+    FILE_PATH = Path(file.filename)
+    WRITE_PATH = WORK_DIR / FILE_PATH
+    with open(str(WRITE_PATH) ,'wb') as myfile:
+        contents = await file.read()
+        myfile.write(contents)
+    # return local file paths
+    a = Inference()
+    result_dir = a.infer(weights='YOLOMODEL/full_10epoch.pt',project="YOLOMODEL/runs/detect",imgsz=[1280,900], source=WRITE_PATH)
+    print(WRITE_PATH)
+    return {"filepath": result_dir}
+    results = Inference.get_results(result_dir)
+    # return local file paths
+    bytes_io = BytesIO()
+    results.save(bytes_io, format='PNG')
+    return Response(bytes_io.getvalue(), media_type="image/png")
+    return {"filepath": str(WRITE_PATH), "source": WORK_DIR}
+##########################<<<<CODE IVAN AND CHRIS HAVE DONE>>>>>################################
+# @ROUTER.post("/preprocess/image", status_code=fastapi.status.HTTP_200_OK)
+# def preprocess_api(file: UploadFile = File(...)): # place holder for image preprocessing 
+#     """Endpoint that takes in the image from user upload and preprocess it for
+#     training or inference.
+
+#     Parameters
+#     ----------
+#     image : Image that user upload for training or inference.
+
+#     Returns
+#     -------
+#     str
+#         address of the preprocessed image
+#     """
+#     image = read_imagefile(file.read())
+#     image.save(uuid.uuid4().hex + r'.jpg')
+#     wk_dir = str(os.getcwd())
+#     print(wk_dir)
+#     return {"address": wk_dir}
+
+
+##############################<FELICIA'S CODE FOR PREDICT#######################################
+# @ROUTER.post("/infer", status_code=fastapi.status.HTTP_200_OK)
+# def predict_model(processed_file_path: str):
+#     """Endpoint that returns dirty classification of floor image.
+
+#     Parameters
+#     ----------
+#     processed_file_path : str
+
+#     Returns
+#     -------
+#     dict
+#         Dictionary containing the prediction for processed image of the request.
+
+#     Raises
+#     ------
+#     fastapi.HTTPException
+#         A 500 status error is returned if the prediction steps
+#         encounters any errors.
+#     """
+#     source = ""
+#     a = Inference()
+#     a.infer(weights="YOLOMODEL/weights/full_10epoch.pt",project="YOLOMODEL/runs/detect",imgsz=[1280,900],source="YOLOMODEL/runs/detect",imgsz=[1280,900],source= source)
+
+#     try:
+#         logger.info("Generating sentiments for floor image.")
+#         curr_pred_result, output_file_path = PRED_MODEL.predict(processed_file_path)
+#         dirt_prediction = "Dirty" if curr_pred_result == 1 else "Clean"
+
+#         logger.info("Prediction generated for Image ID: {}".format(dirt_prediction))
+
+#     except Exception as error:
+#         print(error)
+#         raise fastapi.HTTPException(status_code=500, detail="Internal server error.")
+
+#     return {
+#         "data": {"prediction": dirt_prediction, "image_file_path": output_file_path}
+#         }
+
+############################<<<CODE FEL AND I TRIED>>>##################################
+# https://github.com/davidefiocco/streamlit-fastapi-model-serving/blob/master/fastapi/server.py
+
+# # logger = logging.getLogger(__name__)
+# #PRED_MODEL = team6_miniproject_fapi.deps.PRED_MODEL
+# def get_segments(binary_image):
+
+#     input_image = Image.open(io.BytesIO(binary_image)).convert("RGB")
+
+#     return input_image
+
+# segmented_image = get_segments(file)
+#     bytes_io = io.BytesIO()
+#     segmented_image.save(bytes_io, format='PNG')
+#     return Response(bytes_io.getvalue(), media_type="image/png")
---
+++ src/streamlit-Julia.py
@@ -0,0 +1,113 @@
+import os
+import logging
+import streamlit as st
+import requests
+import aiap_team6_miniproject as a6
+from PIL import Image
+import fastapi
+import io
+import json
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+##############JULIA WORKING CODE##########################################
+
+# # interact with FastAPI endpoint
+test_url = "http://127.0.0.1:8080/api/v1/model/preprocess/image"
+predict_url = "http://127.0.0.1:8080/api/v1/model/infer"
+
+def main():
+    """This main function does the following:
+    - load logging config
+    - loads trained model on cache
+    - gets string input from user to be loaded for inferencing
+    - conducts inferencing on string
+    - outputs prediction results on the dashboard
+    """
+    st.subheader("AIAP Team 6")
+    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+    if image_file is not None:
+        
+        # To See details
+        file_details = {"filename":image_file.name, "filetype":image_file.type,
+                            "filesize":image_file.size}
+        st.write(file_details)
+        # To View Uploaded Image
+        # st.image(Image.open(image_file))
+
+        test_file = image_file.read()
+        # test_file = open(image_file)
+        test_response = requests.post('http://127.0.0.1:8080/preprocess/image', files = {"file": test_file})
+        st.write(test_response)
+        # st.image(test_response)
+
+        if test_response.ok:
+            st.write("Upload completed successfully!")
+        else:
+            st.write("Something went wrong!")
+
+        if st.button("Predictions"):
+            test_response = requests.post('http://127.0.0.1:8080/preprocess/image', files = {"file": test_file})
+            st.write(test_response)
+        
+        st.download_button(
+        label="Download image", 
+        data=image_file,
+        file_name="imagename.png",
+        mime="image/png")
+
+if __name__ == "__main__":
+    main()
+
+##############################################################################
+
+
+# def process(image, server_url: str):
+
+#     m = MultipartEncoder(fields={"file": ("filename", image, "image/jpeg")})
+
+#     r = requests.post(
+#         server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000
+#     )
+
+#     return r
+
+
+# image = st.file_uploader('insert image')
+
+# if st.button('Get segmentation map'):
+#     if image == None:
+#         st.write("Insert an image!")  # handle case with no image
+#     else:
+#         segments = process(image, test_url)
+#         segmented_image = Image.open(io.BytesIO(segments.content)).convert('RGB')
+#         st.image([image, segmented_image], width=300)  # output dyptich
+
+
+
+
+# #@hydra.main(config_path="../conf/base", config_name="pipelines.yml")
+
+
+###############################################################################
+    # logger = logging.getLogger(__name__)
+    # logger.info("Setting up logging configuration.")
+    # logger_config_path = os.path.\
+    #     join(hydra.utils.get_original_cwd(),
+    #         "conf/base/logging.yml")
+    # a6.general_utils.setup_logging(logger_config_path)
+
+    # logger.info("Loading the model...")
+    # pred_model = load_model(args["inference"]["model_path"])
+
+    # logger.info("Loading dashboard...")
+
+        # curr_pred_result = float(pred_model.predict([image_file])[0])
+        # sentiment = ("positive" if curr_pred_result > 0.5
+        #             else "negative")
+        # logger.info(
+        #     "Inferencing has completed. Text input: {}. Sentiment: {}"
+        #     .format(image_file, sentiment))
+        # st.write("The sentiment of the review is {}."
+        #     .format(sentiment))
+    # else:
+    #     st.write("Awaiting a review...")
