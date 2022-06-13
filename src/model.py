778917c9ad27052ed9d935884e0b03bcb1746b1e
Julia Loh JM <julia_loh_jm@aiap.sg>
2022-06-13 17:54:14 +0800

    Updated docstrings

--- src/model.py
+++ src/model.py
@@ -9,27 +9,21 @@ from infer import Inference
 ROUTER = fastapi.APIRouter()
   
 @ROUTER.post("/preprocess/image", status_code=fastapi.status.HTTP_200_OK)
-async def preprocess_api(file: UploadFile=File(...)): # place holder for image preprocessing 
-    """Endpoint that takes in the image from user upload and preprocess it for
-    training or inference.
+async def preprocess_api(file: UploadFile=File(...)):
+    """Takes in the image from user upload and preprocess it for
+    training or inference. Uploaded image is saved in local.
     
-    Parameters
-    ----------
-    image : Image that user upload for training or inference.
+    Args:
+        File (PNG/JPG/JPEG): Image that user upload for training or inference.
 
-    Returns
-    -------
-    str
-        address of the preprocessed image
+    Returns:
+        WRITE_PATH (str): File path of the preprocessed image
     """
     WORK_DIR = os.getcwd()
-    # UUID to prevent file overwrite
-    # 'beautiful' path concat instead of WORK_DIR + '/' + REQUEST_ID
     WORKSPACE = WORK_DIR
     if not os.path.exists(WORKSPACE):
-        # recursively create workdir/unique_id
         os.makedirs(WORKSPACE)
-    # iterate through all uploaded files
+    # save files as unique id
     file.filename = f"{uuid.uuid4()}.jpg"
     FILE_PATH = Path(file.filename)
     WRITE_PATH = WORK_DIR / FILE_PATH
@@ -37,45 +31,37 @@ async def preprocess_api(file: UploadFile=File(...)): # place holder for image p
         contents = await file.read()
         myfile.write(contents)
     return WRITE_PATH
-    # return {"source": str(WRITE_PATH)}
-
 
 @ROUTER.post("/predict", status_code=fastapi.status.HTTP_200_OK)
-async def predict(WRITE_PATH: str = Form(...)): # place holder for image preprocessing 
-    """Endpoint that takes in the image from user upload and preprocess it for
-    training or inference.
+async def predict(WRITE_PATH: str = Form(...)):
+    """Takes in filepath of image uploaded to be fitted into YOLOMODEL and
+    returns filepath of predicted 
+    annotated image
     
-    Parameters
-    ----------
-    image : Image that user upload for training or inference.
+    Args:
+        WRITE_PATH (str): File path of the preprocessed image
 
-    Returns
-    -------
-    str
-        address of the preprocessed image
+    Returns:
+        Image_path (str): File path of the predicted annotated image
     """
     a = Inference()
     result_dir = a.infer(weights='YOLOMODEL/full_10epoch.pt',project="YOLOMODEL/runs/detect",imgsz=[1280,900], source=WRITE_PATH)
-    image = a.get_results(result_dir)
-    # return FileResponse(image)
-    return image
+    Image_path = a.get_results(result_dir)
+    return Image_path
 
 
 @ROUTER.post("/live", status_code=fastapi.status.HTTP_200_OK)
-async def live(WRITE_PATH: str = Form(...)): # place holder for image preprocessing 
-    """Endpoint that takes in the image from user upload and preprocess it for
-    training or inference.
+async def live(WRITE_PATH: str = Form(...)): 
+    """Takes in filepath of image uploaded to be fitted into YOLOMODEL and
+    returns filepath of predicted live webcam
     
-    Parameters
-    ----------
-    image : Image that user upload for training or inference.
+    Args:
+        WRITE_PATH (str): File path of the preprocessed image
 
-    Returns
-    -------
-    str
-        address of the preprocessed image
+    Returns:
+        live_path (str): File path of live webcam
     """
     result_dir = Inference()._run(weights='YOLOMODEL/full_10epoch.pt',project="YOLOMODEL/runs/detect",imgsz=[1280,900], source=0)
-    image = Inference().get_results(result_dir)
-    return image
+    live_image = Inference().get_results(result_dir)
+    return live_image
 
--- src/streamlit.py
+++ src/streamlit.py
@@ -5,21 +5,25 @@ from streamlit_option_menu import option_menu
 import cv2
 
 # # interact with FastAPI endpoint
-test_url = "http://127.0.0.1:8080/preprocess/image"
-predict_url = "'http://127.0.0.1:8080/predict'"
+preprocess_url = "http://127.0.0.1:8080/preprocess/image"
+predict_url = "http://127.0.0.1:8080/predict"
+live_url = "http://127.0.0.1:8080/live"
 
 def main():
     """This main function does the following:
-    - load logging config
+    - format streamlit app
     - loads trained model on cache
-    - gets string input from user to be loaded for inferencing
-    - conducts inferencing on string
-    - outputs prediction results on the dashboard
+    - Gets file/ live webcam image from user to be loaded for inferencing
+    - conducts inferencing on file
+    - outputs predicted annotated image on the dashboard
     """
+    # Header showing "AIAP Team 6 Dirt Detection"
     st.markdown(f'<p style="color:#050505;text-align:center;font-size:48px;border-radius:2%;">AIAP Team 6 Dirt Detection</p>', unsafe_allow_html=True)
 
+    # Subheader showing "You never know how dirty your floor is!"
     st.markdown(f'<p style="background-color:#f50505;color:#f7f5f5;text-align:center;font-size:36px;border-radius:2%;">You never know how dirty your floor is!</p>', unsafe_allow_html=True)
 
+    # Sidebar with options: "Image Prediction" & "Live Detection"
     with st.sidebar:
         selected = option_menu("Main Menu", ["Image Prediction", 'Live Detection'],icons=['image', 'camera'], menu_icon="cast", styles={'nav-link': {'background-color':'#f50505'}}, default_index=1)
 
@@ -31,13 +35,11 @@ def main():
             # To See details
             file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
             st.write(file_details)
-            # To View Uploaded Image
-            test_file = image_file.read()
-            # test_file = open(image_file)
-            test_response = requests.post('http://127.0.0.1:8080/preprocess/image', files = {"file": test_file})
+            # Get response from FASTAPI preprocess_url
+            test_response = requests.post(preprocess_url, files = {"file": image_file.read()})
             saved_address = test_response.text
+            # Prints out saved uploaded file filepath
             st.write("Uploaded file saved at: " + str(saved_address))
-            st.write("This is the original image")
             st.markdown(f'<p style="color:#050505;font-size:22px;border-radius:2%;">Original image uploaded</p>', unsafe_allow_html=True)
             st.image(Image.open(image_file))
 
@@ -47,29 +49,22 @@ def main():
             else:
                 st.write("Something went wrong!")
 
-
-
+            # Predict button
             if st.button("Predictions"):
                 waiting_text = st.empty()
                 waiting_text.text("Generating prediction...")
-                predict = requests.post('http://127.0.0.1:8080/predict', 
+                # Get response from FASTAPI predict_url
+                predict = requests.post(predict_url, 
                 data = {"WRITE_PATH": test_response.json()})
-                st.write(predict.json())
                 st.markdown(f'<p style="color:#050505;font-size:22px;border-radius:2%;">Dirty floor or not?</p>', unsafe_allow_html=True)
                 st.image(Image.open(predict.json()))
-                an_img = predict.json()
-                # saved_address = test_response.text
-                # st.write("Uploaded file saved at: " + str(saved_address))
-            
-                # saved_address = test_response.text
-                # st.write("Uploaded file saved at: " + str(saved_address))
-                with open(an_img, 'rb') as file:
+                
+                with open(predict.json(), 'rb') as file:
                     st.download_button(
                     label="Download image", 
                     data=file,
                     file_name="annotated_image.png",
                     mime="image/png")
-
 
 
     st.markdown('#')
@@ -78,9 +73,9 @@ def main():
         if st.button('Live Detection'):
             waiting_text = st.empty()
             waiting_text.text("Please press 'q' to exit...")
-            requests.post('http://127.0.0.1:8080/live', 
+            # Get response from FASTAPI live_url
+            requests.post(live_url, 
             data = {"WRITE_PATH": 1})   
-            #st.image(Image.open(predict.json()))
             while cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         cv2.destroyAllWindows()
